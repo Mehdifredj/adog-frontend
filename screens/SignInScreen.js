175d23468fd3203b8e2a2a36a0a97d5433d59d13
Nadia <nadiamahmoud.09@gmail.com>
2022-10-24 18:27:50 +0200

    CSS OK : SigninScreen

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,50 +1,56 @@
-
-import React from 'react'
-import {useState} from'react';
-import { useDispatch } from 'react-redux';
-import { login } from '../reducers/user';
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../reducers/user";
 import {
-    Image, KeyboardAvoidingView, Platform,
-    StyleSheet, Text, View, ImageBackground,
-    TextInput, TouchableOpacity} from "react-native";
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
-
-export default function SignInScreen({navigation}) {
-
+export default function SignInScreen({ navigation }) {
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState(''); //initialisation des etats pour l'email et le passxord
-  const [password, setPassword] = useState('');
-
+  const [email, setEmail] = useState(""); //initialisation des etats pour l'email et le passxord
+  const [password, setPassword] = useState("");
 
   const handleConnection = () => {
-
-		fetch('http://192.168.10.203:3000/users/signin', { // requete fetch avec notre adresse IP personnelle sur la route POST signin
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email: email, password: password }),
-		}).then(response => response.json())
-			.then(data => {
-				if (data.result) {
-					dispatch(login({ email: email }));   // dispatch pour stocker les données dans le reducer
-          setEmail('');
-					setPassword('');
-          navigation.navigate('UserProfile'); // permet la redirection vers la page userProfile. 
-				}
-			});
-	};
+    fetch("http://192.168.10.203:3000/users/signin", {
+      // requete fetch avec notre adresse IP personnelle sur la route POST signin
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email, password: password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(login({ email: email })); // dispatch pour stocker les données dans le reducer
+          setEmail("");
+          setPassword("");
+          navigation.navigate("UserProfile"); // permet la redirection vers la page userProfile.
+        }
+      });
+  };
 
   return (
-
-     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <Image style={styles.imageLogo} source={require('../images/logo.jpg')} />
-      <Text style={styles.title}>SignInScreen</Text>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Image style={styles.imageLogo} source={require("../images/logo.jpg")} />
 
       <TextInput
         placeholder="email"
         onChangeText={(value) => setEmail(value)}
         value={email}
-        style={styles.input}/>
+        style={styles.input}
+      />
 
       <TextInput
         placeholder="password"
@@ -52,52 +58,52 @@ export default function SignInScreen({navigation}) {
         value={password}
         style={styles.input}
       />
-       
-      <TouchableOpacity onPress={() => handleConnection()}>
-       <Image style={styles.imagestyle} source={require('../images/Logo-GO.jpg')}/>
-      </TouchableOpacity>
-     </KeyboardAvoidingView>
-  )
-}
-  
-const styles = StyleSheet.create({
 
+      <TouchableOpacity onPress={() => handleConnection()}>
+        <Text style={styles.titleLogoGO}>Go!</Text>
+        <Image
+          style={styles.imagestyleLogoGo}
+          source={require("../images/Logo-GO.jpg")}
+        />
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'white',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
   },
 
   imageLogo: {
-<<<<<<< HEAD
-    width: "30%",
-    height: "30%",
-    borderRadius:10,
-=======
     width: 300,
     height: 300,
-    borderRadius:"30%",
->>>>>>> 09c8d2d7f1952f7d59f343f2494e7913648cd16a
+    borderRadius: "30%",
   },
-  
+
   input: {
-    width: '80%',
+    width: "80%",
     marginTop: 25,
-    borderBottomColor: '#F1890F',
+    borderBottomColor: "#F1890F",
     borderBottomWidth: 1,
     fontSize: 18,
   },
 
-  textButton: {
-    color: '#ffffff',
-    height: 30,
-    fontWeight: '600',
-    fontSize: 16,
+  imagestyleLogoGo: {
+    height: 150,
+    width: 150,
   },
-  imagestyle:{
 
-height: 150,
-width: 150,
+  titleLogoGO: {
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#F1890F",
+    fontSize: 20,
+    fontWeight: "600",
+    marginLeft: 65,
+    marginTop: 120,
   },
 });
