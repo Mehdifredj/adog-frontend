84151356378a1a996e5d433a72d83f4fa3f4bee5
capucine <capucine.mahe.oc@gmail.com>
2022-10-28 16:05:04 +0200

    user profile front ok

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function FilterScreen() {
+export default function ChatScreen() {
   return (
     <View>
-      <Text>Filtres</Text>
+      <Text>ChatScreen</Text>
     </View>
   )
 }
