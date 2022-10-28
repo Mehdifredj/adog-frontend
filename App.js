36bf18e163307274a97a6a8770aae430441748f9
capucine <capucine.mahe.oc@gmail.com>
2022-10-28 17:24:24 +0200

    capucine 17:24

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-import user from './reducers/user';
+import user from "./reducers/user";
 
-
-import FiltersScreen from './screens/FiltersScreen';
-import SwipesScreen from './screens/SwipesScreen';
-import HomeScreen from './screens/HomeScreen';
-import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import ChatScreen from './screens/ChatScreen';
-import UserProfileScreen from './screens/UserProfileScreen';
-
+import FiltersScreen from "./screens/FiltersScreen";
+import SwipesScreen from "./screens/SwipesScreen";
+import HomeScreen from "./screens/HomeScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import ChatScreen from "./screens/ChatScreen";
+import UserProfileScreen from "./screens/UserProfileScreen";
 
 const store = configureStore({
   reducer: { user },
@@ -28,46 +26,46 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName = '';
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
 
-        if (route.name === 'Swipes') {
-          iconName = 'heart';
-        } else if (route.name === 'Chat') {
-          iconName = 'comments'; 
-        } else if(route.name ==='Profile'){
-          iconName = 'paw'; 
-        }else if(route.name==='Filters'){
-          iconName='sliders';
-        }
-        return <FontAwesome name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: '#F1890F',
-      tabBarInactiveTintColor: '#9c9c9c',
-      headerShown: false,
-    })}>
-       <Tab.Screen name="SignUp" component={SignUpScreen} />
-       <Tab.Screen name="SignIn" component={SignInScreen} />
-      <Tab.Screen name="Filters" component={FiltersScreen}/>
-      <Tab.Screen name="Swipes" component={SwipesScreen}/>
-       <Tab.Screen name="Chat" component={ChatScreen} /> 
-       <Tab.Screen name="Profile" component={UserProfileScreen} />
-    
-
+          if (route.name === "Swipes") {
+            iconName = "heart";
+          } else if (route.name === "Chat") {
+            iconName = "comments";
+          } else if (route.name === "Profile") {
+            iconName = "paw";
+          } else if (route.name === "Filters") {
+            iconName = "sliders";
+          }
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#F1890F",
+        tabBarInactiveTintColor: "#9c9c9c",
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="SignUp" component={SignUpScreen} />
+      <Tab.Screen name="SignIn" component={SignInScreen} />
+      <Tab.Screen name="Filters" component={FiltersScreen} />
+      <Tab.Screen name="Swipes" component={SwipesScreen} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+      <Tab.Screen name="Profile" component={UserProfileScreen} />
     </Tab.Navigator>
   );
 };
 
 export default function App() {
   return (
-     <Provider store={store}>
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="TabNavigator" component={TabNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
-     </Provider>
+    </Provider>
   );
 }
